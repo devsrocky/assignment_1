0d4520fc58d7ba731f3ff95e4545feeafed69a4a
devsrocky <devsrocky1@gmail.com>
2024-01-09 22:29:53 +0600

    my changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,15 @@
 let navOp =  document.querySelector('.openNv')
 let navCl =  document.querySelector('.closeNv')
 let navIt = document.querySelector('.nav_items')
+let bodyOv= document.querySelector('body');
 
 navOp.addEventListener('click', function() {
     navCl.classList.add('active')
     navIt.classList.add('active')
+    bodyOv.classList.add('active')
 })
 navCl.addEventListener('click', function() {
     navCl.classList.remove('active')
     navIt.classList.remove('active')
-
+    bodyOv.classList.remove('active')
 })
